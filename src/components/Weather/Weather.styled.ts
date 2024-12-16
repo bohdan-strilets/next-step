0e9e12ef0552d23b470d0016b8d2c02c1bb14fd9
BS <bohdan.strilets@gmail.com>
2024-12-16 14:49:28 +0100

    Made the weather component responsive to different screens.

--- src/components/Weather/Weather.styled.ts
+++ src/components/Weather/Weather.styled.ts
@@ -3,6 +3,8 @@ import { motion } from 'framer-motion'
 
 import rainyBg from '@/assets/images/weather/rainy-bg.jpg'
 
+import { screenWidth } from '@/utils/data/screenWidth'
+
 export const Wrapper = styled.div`
 	position: relative;
 
@@ -19,37 +21,76 @@ export const Wrapper = styled.div`
 export const WeatherForecast = styled.div`
 	position: absolute;
 	bottom: 10%;
-	left: 10%;
-
-	display: flex;
-	align-items: center;
-	justify-content: center;
+	left: 3%;
 
 	color: var(--white-color);
+
+	@media screen and (min-width: ${screenWidth.TABLET}) {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		left: 5%;
+	}
+
+	@media screen and (min-width: ${screenWidth.LAPTOP}) {
+		left: 8%;
+	}
+
+	@media screen and (min-width: ${screenWidth.DESKTOP}) {
+		left: 10%;
+	}
 `
 
 export const Temperature = styled(motion.p)`
-	font-size: 124px;
+	font-size: 64px;
 	font-weight: 700;
+
+	@media screen and (min-width: ${screenWidth.TABLET}) {
+		font-size: 84px;
+	}
+
+	@media screen and (min-width: ${screenWidth.LAPTOP}) {
+		font-size: 124px;
+	}
 `
 
 export const LocationWrapper = styled(motion.div)`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+	margin: 0;
 
-	margin: 0 25px;
+	@media screen and (max-width: ${screenWidth.PRE_TABLET}) {
+		margin-bottom: 30px;
+	}
+
+	@media screen and (min-width: ${screenWidth.TABLET}) {
+		margin: 0 25px;
+	}
 `
 
 export const Location = styled.p`
-	font-size: 62px;
+	font-size: 48px;
 	font-weight: 600;
 	line-height: 1.2;
+
+	@media screen and (min-width: ${screenWidth.TABLET}) {
+		font-size: 52px;
+	}
+
+	@media screen and (min-width: ${screenWidth.LAPTOP}) {
+		font-size: 62px;
+	}
 `
 
 export const DateTime = styled.p``
 
 export const IconWrapper = styled(motion.div)`
-	font-size: 64px;
+	font-size: 54px;
+
+	@media screen and (min-width: ${screenWidth.TABLET}) {
+		font-size: 58px;
+	}
+
+	@media screen and (min-width: ${screenWidth.LAPTOP}) {
+		font-size: 64px;
+	}
 `
